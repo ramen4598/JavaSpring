61b3866dbedab3859fa7f3672bc0bffeffc6a819
tired_i <ramen4598@naver.com>
2023-09-08 13:59:12 +0900

    MyCalculator에서 ICalculator를 매개변수로 받도록 수정

--- project/calc_2/src/calc_1/MainClass.java
+++ project/calc_2/src/calc_1/MainClass.java
@@ -4,9 +4,9 @@ public class MainClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		MyCalculator calculator = new MyCalculator();
-		calculator.calAdd(10,5, new CalAdd());
-		calculator.calSub(10,5, new CalSub());
-		calculator.calMul(10,5, new CalMul());
-		calculator.calDiv(10,5, new CalDiv());
+		calculator.calculate(10,5, new CalAdd());
+		calculator.calculate(10,5, new CalSub());
+		calculator.calculate(10,5, new CalMul());
+		calculator.calculate(10,5, new CalDiv());
 	}
 }
--- project/calc_2/src/calc_1/MyCalculator.java
+++ project/calc_2/src/calc_1/MyCalculator.java
@@ -1,27 +1,8 @@
 package calc_1;
 
 public class MyCalculator {
-	
-	public void calAdd(int fNum, int sNum, CalAdd calAdd){
-		//ICalculator calculator = new CalAdd();
-		int value = calAdd.doOperation(fNum, sNum);
+	public void calculate(int fNum, int sNum, ICalculator calculator){
+		int value = calculator.doOperation(fNum, sNum);
 		System.out.println("result : " + value);
 	}
-	
-	public void calSub(int fNum, int sNum, CalSub calSub){
-		//ICalculator calculator = new CalSub();
-		int value = calSub.doOperation(fNum, sNum);
-		System.out.println("result : " + value);
-	}
-	public void calMul(int fNum, int sNum, CalMul calMul){
-		//ICalculator calculator = new CalMul();
-		int value = calMul.doOperation(fNum, sNum);
-		System.out.println("result : " + value);
-	}
-	public void calDiv(int fNum, int sNum, CalDiv calDiv){
-		//ICalculator calculator = new CalDiv();
-		int value = calDiv.doOperation(fNum, sNum);
-		System.out.println("result : " + value);
-	}
-
 }
